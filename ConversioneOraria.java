538db14f649ac6520cd1b2f9798c1e5415b4330e
Delfino <simone.orlando.007@gmail.com>
2025-02-12 21:30:14 +0100

    Completato esercizio 2

--- ConversioneOraria.java
+++ ConversioneOraria.java
@@ -6,6 +6,11 @@ public class ConversioneOraria
 
 		System.out.println("Inserisci un orario in secondi: ");
 		int tempo = input.nextInt();
+		while(!Grandezza(tempo)){
+		        System.out.println("Inserisci un numero maggiore di 0 e minore di 84600");
+		        tempo = input.nextInt();
+		    }
+		System.out.println(Conversione(tempo));
 	}
 
 	public static boolean Grandezza(int numero){
@@ -17,10 +22,23 @@ public class ConversioneOraria
 				}
 		}
 
-	public static int Conversione(int numero){
-			
-		
-
-			return
-		}
+	public static String Conversione(int numero) {
+            String tempo = "";
+            if (numero > 60) { // Se è più grande dei secondi
+                if (numero > 3600) { // Se è più grande di 60 minuti
+                    int ore = numero / 3600;
+                    int minuti = (numero % 3600) / 60;
+                    int secondi = numero % 60;
+                    tempo = ore + " h, " + minuti + " m, " + secondi + " s";
+                } else if (numero <= 3600) {
+                    int minuti = numero / 60;
+                    int secondi = numero % 60;
+                    tempo = "0 h, " + minuti + " m, " + secondi + " s";
+                }
+            }
+            if (numero <= 60) {
+                tempo = "0 h, 0 m, " + numero + " s";
+            }
+            return tempo;
+        }
 }
