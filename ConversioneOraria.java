0ac4c17162a3de1c3902dc1fd411bacf4e10c1f4
delfinx <simone.orlando.007@gmail.com>
2025-02-12 08:31:38 +0100

    Iniziato esercizio 2

---
+++ ConversioneOraria.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ConversioneOraria
+{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Inserisci un orario in secondi: ");
+		int tempo = input.nextInt();
+	}
+
+	public static boolean Grandezza(int numero){
+			if(numero > 84600 || numero < 0){
+					return false;
+				}
+			else{
+					return true;
+				}
+		}
+
+	public static int Conversione(int numero){
+			
+		
+
+			return
+		}
+}
